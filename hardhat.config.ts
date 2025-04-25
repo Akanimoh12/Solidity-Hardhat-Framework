c0e35a20d66e88c849148086b5c207c394e47ec5
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-04-25 18:11:50 +0100

    updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,15 +4,15 @@ require("dotenv").config({ path: ".env" });
 
 module.exports = {
   solidity: "0.8.20",
-  networks: {
-    sepolia: {
-      url: process.env.SEPOLIA_URL,
+  // networks: {
+  //   sepolia: {
+  //     url: process.env.SEPOLIA_URL,
 
-      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
-    },
-  },
+  //     accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+  //   },
+  // },
 
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY,
+  // },
 };
